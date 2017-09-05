bd07b656f9ee1b01cf2f77dbd427069ce0b783b7
Mike Wills <mike@mikewills.me>
2017-09-04 23:59:25 -0500

    Alerts seem to be working.
    I just need to figure out the HWO yet.

--- js/index.js
+++ js/index.js
@@ -29,6 +29,10 @@ var weatherViewModel = {
 	gridY: ko.observable(0),
 	locationName: ko.observable(""),
 	radarStation: ko.observable(""),
+	stationUrl: ko.observable(""),
+	stationImg: ko.observable(""),
+	radarUrl: ko.observable(""),
+	radarImg: ko.observable(""),
 	periods: ko.observableArray([])
 };
 
@@ -40,6 +44,10 @@ $.get('https://api.weather.gov/points/' + coordinates, function(json) {
 	weatherViewModel.gridY(json.properties.gridY);
 	var locationName = "Forecast for: " + json.properties.relativeLocation.properties.city + " " + json.properties.relativeLocation.properties.state
 	weatherViewModel.locationName(locationName);
+	weatherViewModel.stationUrl("https://www.weather.gov/" + json.properties.cwa + "/");
+	weatherViewModel.stationImg("https://www.weather.gov/wwamap/png/" + json.properties.cwa.toLowerCase() + ".png");
+	weatherViewModel.radarUrl("https://radar.weather.gov/radar.php?rid=" + json.properties.cwa + "&product=N0R&overlay=11101111&loop=no");
+	weatherViewModel.radarImg("https://radar.weather.gov/lite/N0R/" + json.properties.cwa + "_0.png");
 	
 	//console.log("URL: " + json.properties.forecast);
 	
@@ -47,9 +55,27 @@ $.get('https://api.weather.gov/points/' + coordinates, function(json) {
 		//console.log("Call 2: " + JSON.stringify(forecastJson));
 		weatherViewModel.periods(forecastJson.properties.periods);
 	});
+	
+	$.get(json.properties.county, function(countyJson){
+		var zone = countyJson.properties.id;
+		
+		$.get("https://api.weather.gov/alerts?active=1&zone=" + zone, function(alertsJson){
+			alertsViewModel.features(alertsJson.features);
+		});
+		
+	});
 });
 
 ko.applyBindings(weatherViewModel, document.getElementById('future-weather'));
+
+
+// ==================
+// Current Weather View Model
+// ==================
+var alertsViewModel = {
+	features: ko.observableArray([])
+};
+ko.applyBindings(weatherViewModel, document.getElementById('weather-alerts'));
 
 
 // ==================
@@ -72,3 +98,14 @@ var currentWeatherViewModel = {
 }
 
 //ko.applyBindings(weatherViewModel, document.getElementById('current-weather'));
+
+ko.bindingHandlers.lineBreaks = {
+    init: function (element, valueAccessor, allBindings, data, context) {  
+	    var value = valueAccessor();           
+        $(element).html(value.replace(/\n/g, '<br />'));      
+    },
+    update: function (element, valueAccessor, allBindings, data, context) {
+    	var value = valueAccessor();    
+        $(element).html(value.replace(/\n/g, '<br />'));      
+    }
+};
